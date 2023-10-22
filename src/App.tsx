4d987c76dc0e256f93d9bd06e1bfacd5f508d8fb
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-22 10:56:11 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import '@mantine/core/styles.css';
-import { Button } from '@mantine/core';
 
 import { MantineProvider } from '@mantine/core';
 
 function App() {
 	return (
 		<MantineProvider>
-			<h1 className='text-3xl font-bold underline'>Hello</h1>
-			<Button variant='filled' className='bg-red-300'>
-				Button
-			</Button>
-			;
+			<div id='App'>Password Generator</div>
 		</MantineProvider>
 	);
 }
