c61345640a2fb6cf82da2362e976598ef8edffb2
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-30 11:36:23 +0300

    new generate function

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -1,61 +1,31 @@
-interface IRandomFunc {
-	[key: string]: () => string;
-}
+function generate(
+	length: number,
+	lower: boolean,
+	upper: boolean,
+	number: boolean,
+	symbol: boolean
+): string {
+	let password = '';
+	const lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+	const upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const numberChars = '0123456789';
+	const symbolChars = '!@#$%^&*()_+-=[]{}|;:,.<>?';
 
-const randomFunc: IRandomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol,
-};
+	let chars = '';
+	if (lower) chars += lowerChars;
+	if (upper) chars += upperChars;
+	if (number) chars += numberChars;
+	if (symbol) chars += symbolChars;
 
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.';
-	return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-interface IGenerate {
-	length: number;
-	lower: boolean;
-	upper: boolean;
-	number: boolean;
-	symbol: boolean;
-}
-
-const generate = ({ length, lower, upper, number, symbol }: IGenerate) => {
-	let generatedPassword: string = '';
-	const typesCount: number =
-		(lower ? 1 : 0) + (upper ? 1 : 0) + (number ? 1 : 0) + (symbol ? 1 : 0);
-	const typesArr: string[] = [
-		lower ? 'lower' : '',
-		upper ? 'upper' : '',
-		number ? 'number' : '',
-		symbol ? 'symbol' : '',
-	].filter(item => Object.values(item)[0]);
-	if (typesCount === 0) {
-		return '';
-	}
-	for (let i = 0; i < length; i += typesCount) {
-		typesArr.forEach(type => {
-			const funcName = Object.keys(type)[0];
-			generatedPassword += randomFunc[funcName]();
-		});
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * chars.length);
+		password += chars[randomIndex];
 	}
 
-	const finalPassword = generatedPassword.slice(0, length);
-	return finalPassword;
-};
+	return password;
+}
 
 export default generate;
+
+// create a generate fuction that takes in the length, lower, upper, number, symbol and returns a string of the password
+// function should generate random password. If options are true then add that type of character to the password
